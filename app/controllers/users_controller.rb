f7aaf070462aecd6de43390c7c3f1eac446b87d1
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-12-03 17:49:16 +0300

    completed assgnment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
-    skip_before_action :authorize, only: :create
-  
-    def create
+  def create
       user = User.create!(user_params)
       session[:user_id] = user.id
       render json: user, status: :created
-    end
-  
-    def show
-      render json: @current_user
-    end
-  
-    private
-  
-    def user_params
-      params.permit(:username, :password, :password_confirmation, :image_url, :bio)
-    end
-  
   end
+
+  def show
+      user = User.find_by(id: session[:user_id])
+      if user
+          render json: user
+      else
+          render json: {error: "User not found"}, status: :unauthorized
+      end
+  end
+  private
+  def user_params
+      params.permit(:username, :password, :password_confirmation, :bio, :image_url)
+  end
+end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
-    belongs_to :user
-  
-    validates :title, presence: true
-    validates :instructions, length: { minimum: 50 }
-  end
+  validates :title, presence: true
+  validates :instructions, length: {minimum: 50}
+  validates :minutes_to_complete, presence: true
+  belongs_to :user
+
+end
